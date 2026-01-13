13394f80fff64faca5f4910477b71ced05fee00c
teddinata <teddinataa@gmail.com>
2026-01-13 10:32:44 +0700

    fix: update asset paths for ProductScreen.js and pos_margin.js in manifest

--- __manifest__.py
+++ __manifest__.py
@@ -28,8 +28,8 @@ Features:
         'point_of_sale.assets': [
             # 'pos_stock_display/static/src/css/pos_stock.css',
             # 'pos_stock_display/static/src/js/models.js',
-            'pos_stock_display/static/src/js/ProductScreen.js',
-            'pos_stock_display/static/src/js/pos_margin.js',
+            'odoo-pos-pitcar/static/src/js/ProductScreen.js',
+            'odoo-pos-pitcar/static/src/js/pos_margin.js',
         ],
     },
     'installable': True,
